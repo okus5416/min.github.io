e46b3c1aa8e5ece723a4585c5a3660c6b8cbb225
Min Suk Kim <okus5416@gmail.com>
2016-05-12 17:05:29 -0400

    modified

---
+++ jquery.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function() {
+                  
+                  $("h1").fadeToggle(1000).fadeToggle(1000);
+                  $("h2").fadeToggle(1000).fadeToggle(1000);
+                  $(".subtitle").click(function(){
+                                       $(".subtitle").text("Done by jQuery!");
+                                       });
+                  });
+                  
+                  
